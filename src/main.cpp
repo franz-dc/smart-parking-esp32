99b16df06a91dd3b2f8107f914924c42b7da6faa
Franz Joshua Dela Cruz <franzdc.21@gmail.com>
2022-05-22 21:30:09 +0800

    Optimize code for scale up to 3 sets

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,11 @@
 #include <secrets.h>
 #include <string.h>
 
+// Define the number of sets for the microcontroller.
+// If this is less than 3, set the pins at the bottom as empty string or 0
+// accordingly.
+#define TOTAL_SETS 3
+
 // set 1
 #define LOT_NUMBER_1 "1A-001"
 #define RED_LED_PIN_1 2
@@ -15,11 +20,19 @@
 
 // set 2
 #define LOT_NUMBER_2 "1A-002"
-#define RED_LED_PIN_2 21
-#define GREEN_LED_PIN_2 22
-#define BLUE_LED_PIN_2 23
+#define RED_LED_PIN_2 17
+#define GREEN_LED_PIN_2 18
+#define BLUE_LED_PIN_2 19
 #define ECHO_PIN_2 32
 #define TRIG_PIN_2 33
+
+// set 3
+#define LOT_NUMBER_3 "1A-003"
+#define RED_LED_PIN_3 21
+#define GREEN_LED_PIN_3 22
+#define BLUE_LED_PIN_3 23
+#define ECHO_PIN_3 27
+#define TRIG_PIN_3 14
 
 #define SEND_POLLING_RATE_MS 1000
 #define RECEIVE_POLLING_RATE_MS 3000
@@ -34,16 +47,19 @@ FirebaseConfig config;
 // variable declarations
 bool isReserved = false;
 bool isWithinRange = false;
-bool occupiedStatuses[] = {false, false};
-bool reservedStatuses[] = {false, false};
-short ledPins[][3] = {
+bool occupiedStatuses[3] = {false, false, false};
+bool reservedStatuses[3] = {false, false, false};
+short ledPins[3][3] = {
   {RED_LED_PIN_1, GREEN_LED_PIN_1, BLUE_LED_PIN_1},
-  {RED_LED_PIN_2, GREEN_LED_PIN_2, BLUE_LED_PIN_2}};
+  {RED_LED_PIN_2, GREEN_LED_PIN_2, BLUE_LED_PIN_2},
+  {RED_LED_PIN_3, GREEN_LED_PIN_3, BLUE_LED_PIN_3}};
+short trigPins[3] = {TRIG_PIN_1, TRIG_PIN_2, TRIG_PIN_3};
+short echoPins[3] = {ECHO_PIN_1, ECHO_PIN_2, ECHO_PIN_3};
 unsigned long sendDataPrevMillis = 0;
 unsigned long receiveDataPrevMillis = 0;
 float newDistance = 0;
 String uid = "";
-String lotNumbers[] = {LOT_NUMBER_1, LOT_NUMBER_2};
+String lotNumbers[] = {LOT_NUMBER_1, LOT_NUMBER_2, LOT_NUMBER_3};
 
 void initWifi() {
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
@@ -62,20 +78,19 @@ void setLed(short ledPins[3], short red, short green, short blue) {
   analogWrite(ledPins[2], blue);
 }
 
-void getSensorInfo(int sensorNumber, short trigPin, short echoPin) {
+void getSensorInfo(int sensorNumber) {
   // generate 10-us pulse to TRIG pin
-  digitalWrite(trigPin, HIGH);
+  digitalWrite(trigPins[sensorNumber], HIGH);
   delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
+  digitalWrite(trigPins[sensorNumber], LOW);
 
   // get sensor data: distance in cm
-  newDistance = pulseIn(echoPin, HIGH) / 29.1 / 2;
+  newDistance = pulseIn(echoPins[sensorNumber], HIGH) / 29.1 / 2;
 
   // check if distance is within range
   isWithinRange = newDistance <= MAX_DISTANCE_CM;
 
   // outputs
-
   if (isWithinRange) {
     setLed(ledPins[sensorNumber], 255, 0, 0);
   } else {
@@ -148,37 +163,31 @@ void setup() {
   uid = auth.token.uid.c_str();
   Serial.print("User UID: " + uid + "\n");
 
-  // Pin setup 1
-  pinMode(RED_LED_PIN_1, OUTPUT);
-  pinMode(GREEN_LED_PIN_1, OUTPUT);
-  pinMode(BLUE_LED_PIN_1, OUTPUT);
-  pinMode(ECHO_PIN_1, INPUT);
-  pinMode(TRIG_PIN_1, OUTPUT);
-
-  // Pin setup 2
-  pinMode(RED_LED_PIN_2, OUTPUT);
-  pinMode(GREEN_LED_PIN_2, OUTPUT);
-  pinMode(BLUE_LED_PIN_2, OUTPUT);
-  pinMode(ECHO_PIN_2, INPUT);
-  pinMode(TRIG_PIN_2, OUTPUT);
+  // Set up pins for each set
+  for (short set = 0; set < TOTAL_SETS; set++) {
+    pinMode(ledPins[set][0], OUTPUT);
+    pinMode(ledPins[set][1], OUTPUT);
+    pinMode(ledPins[set][2], OUTPUT);
+    pinMode(trigPins[set], OUTPUT);
+    pinMode(echoPins[set], INPUT);
+  }
 }
 
 void loop() {
   if (Firebase.ready()) {
     if (millis() - sendDataPrevMillis > SEND_POLLING_RATE_MS || sendDataPrevMillis == 0) {
       sendDataPrevMillis = millis();
-
-      getSensorInfo(0, TRIG_PIN_1, ECHO_PIN_1);
-      getSensorInfo(1, TRIG_PIN_2, ECHO_PIN_2);
-
+      for (short set = 0; set < TOTAL_SETS; set++) {
+        getSensorInfo(set);
+      }
       Serial.println("----------------------------------------");
     }
 
     if (millis() - receiveDataPrevMillis > RECEIVE_POLLING_RATE_MS || receiveDataPrevMillis == 0) {
       receiveDataPrevMillis = millis();
-
-      getReservationStatus(0);
-      getReservationStatus(1);
+      for (short set = 0; set < TOTAL_SETS; set++) {
+        getReservationStatus(set);
+      }
     }
   }
 }
